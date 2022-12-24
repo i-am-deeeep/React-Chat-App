f36ca2bb269220f6fe55850fdb4185b07a95271b
Subhodeep Kumar Singh <69170359+i-am-deeeep@users.noreply.github.com>
2022-12-25 02:01:43 +0530

    Added a bit

--- chat_application/src/App.js
+++ chat_application/src/App.js
@@ -6,7 +6,8 @@ const App = () => {
     return(
         <ChatEngine
             height="100vh"
-            projectID="82ef0f1f-51cb-4d24-a646-e4947eab222f"
+            projectID="82ef0f1f-51cb-4d24-a646-e4947eab222f
+            -51cb-4d24-a646-e4947eab222f"
             userName="DEEP"
             userSecret="123123"
             renderChatFeed= {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
--- chat_application/src/components/ChatFeed.jsx
+++ chat_application/src/components/ChatFeed.jsx
@@ -3,12 +3,41 @@ import MyMessage from './MyMessage'
 import TheirMessage from './TheirMessage'
 
 const ChatFeed = (props) =>{
+    
     const { chats,activeChat, username, messages} = props
     const chat=chats && chats[activeChat]
-    console.log(chat)
+    
+    const RenderMessages = () =>{
+        
+        const keys=Object.keys(messages)
+        
+        return keys.map((key,index) => {
+        
+            const message=messages[key]
+            const lastMessageKey= index===0 ? null : keys[index-1]
+            const isMyMessage= username===message.sender.username
+
+            return (
+                <div key={`msg_${index}`} style={{width :'100%'}} >
+                    <div className='message-block'>
+                        {
+                        isMyMessage 
+                        ? <MyMessage/> 
+                        : <TheirMessage/>
+                        }
+                    </div>
+                    <div className="read-receipts" style={{marginLeft: isMyMessage ? '18px' : '0px',marginRight: isMyMessage ? '0px' : '68px'}}>
+                        read-receipts
+                    </div>
+                </div>
+            )
+
+        })
+    }
+    RenderMessages()
     return (
-        <div>
-            Yaha pe karenge
+        <div className='chat-feed'>
+            <div className=""></div>
         </div>
     )
 }
